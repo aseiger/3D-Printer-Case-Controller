c339f07200d52bbfbe2cf2c8f69c550228502814
Alex Seiger <awseiger@gmail.com>
2020-04-05 17:48:01 -0400

    Bare Servo and Temperature sensor control classes

--- 3dPrinterCaseController.cpp
+++ 3dPrinterCaseController.cpp
@@ -1,21 +1,34 @@
 #include <Arduino.h>
-#include "OneWire\OneWire.h"
-#include "Arduino-Temperature-Control-Library/DallasTemperature.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Servo.h>
+
 #include "TemperatureSensor.h"
+#include "ValveControl.h"
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
+// Servo Pin
+#define SERVO_PIN 3
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 TemperatureSensor temperatureSensor(&oneWire);
+
+//set up the valve control servo
+ValveControl butterflyValve(SERVO_PIN);
+
+double valvePosition = 0.25;
 
 void setup() {
 	pinMode(13, OUTPUT);
 	Serial.begin(115200);
 	Serial.println("Starting...");
 	temperatureSensor.begin();
-	temperatureSensor.setPollRate(5000);//500ms update rate
+	temperatureSensor.setPollRate(2500);//500ms update rate
+
+	butterflyValve.begin();
+	butterflyValve.SetValvePosition(0.5);
 }
 
 void loop() {
@@ -23,5 +36,14 @@ void loop() {
 	if(temperatureSensor.hasNewTemperature())
 	{
 		Serial.println(temperatureSensor.getTemperature_C());
+
+		//update valve positon, because we can?
+		if(valvePosition < 0.5)
+			valvePosition = 0.75;
+		else
+			valvePosition = 0.25;
+
+		butterflyValve.SetValvePosition(valvePosition);
+
 	}
 }
--- build/Arduino Uno/project/3dPrinterCaseController.cpp.d
+++ build/Arduino Uno/project/3dPrinterCaseController.cpp.d
@@ -10,7 +10,9 @@ project/3dPrinterCaseController.cpp.o: ../../3dPrinterCaseController.cpp \
  C:/Users/awsei/.arduinocdt/packages/arduino/hardware/avr/1.8.2/cores/arduino/USBAPI.h \
  C:/Users/awsei/.arduinocdt/packages/arduino/hardware/avr/1.8.2/cores/arduino/Arduino.h \
  C:/Users/awsei/.arduinocdt/packages/arduino/hardware/avr/1.8.2/variants/standard/pins_arduino.h \
- ../../OneWire\OneWire.h ../../OneWire\util/OneWire_direct_regtype.h \
- ../../Arduino-Temperature-Control-Library/DallasTemperature.h \
- ../../Arduino-Temperature-Control-Library/..\OneWire\OneWire.h \
- ../../TemperatureSensor.h
+ C:/Users/awsei/.arduinocdt/libraries/OneWire/2.3.5/OneWire.h \
+ C:/Users/awsei/.arduinocdt/libraries/OneWire/2.3.5/util/OneWire_direct_regtype.h \
+ C:/Users/awsei/.arduinocdt/libraries/DallasTemperature/3.8.0/DallasTemperature.h \
+ C:/Users/awsei/.arduinocdt/libraries/Servo/1.1.6/src/Servo.h \
+ C:/Users/awsei/.arduinocdt/libraries/Servo/1.1.6/src/avr/ServoTimers.h \
+ ../../TemperatureSensor.h ../../ValveControl.h
